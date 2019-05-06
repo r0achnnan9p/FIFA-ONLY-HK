aa2fa3dbe4a35f054e613d86b046cbb15b02d1d0
r0achnnan9p <35089045+r0achnnan9p@users.noreply.github.com>
2019-05-06 23:02:10 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,31 @@
+internal_interface=$(ip route|egrep "dev br0|dev br-lan"|head -1|cut -d" " -f3)
+iptables -t filter -D FORWARD -i $internal_interface -j easucks-FORWARD 2>/dev/null
+iptables -t filter -F easucks-FORWARD 2>/dev/null
+iptables -t filter -X easucks-FORWARD 2>/dev/null
+iptables -t filter -N easucks-FORWARD
+iptables -t filter -I FORWARD -i $internal_interface -j easucks-FORWARD
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 109.200.215.132/32 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 109.200.215.140/32 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 109.200.221.170/31 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 159.153.165.200/31 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 159.153.28.50/31 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 159.153.36.60/30 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 159.153.42.240/31 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 169.38.100.162/32 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 169.38.100.170/32 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 169.57.76.162/32 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 169.57.76.171/32 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 18.184.251.35/32 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 18.196.167.42/32 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 18.197.48.114/32 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 185.179.200.211/32 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 185.179.200.226/32 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 185.179.203.68/32 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 185.179.203.80/32 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 185.50.104.206/32 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 185.50.104.221/32 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 203.195.120.68/32 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 203.195.122.124/32 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 52.58.40.163/32 -j DROP
+iptables -t filter -A easucks-FORWARD -i $internal_interface -p udp -d 103.194.165.14/32 -j DROP
+echo "Done!"
